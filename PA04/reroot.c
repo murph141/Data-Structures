bc816b2783076a7198ebc1075aae71db1cc594cb
Eric Murphy <murph141@purdue.edu>
2014-03-29 20:48:11 +0000

    Pop operation not working

---
+++ PA04/reroot.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "reroot.h"
+
+int main(int argc, char * argv[])
+{
+  if(argc != 3)
+  {
+    printf("Usage: proj4 <input_file> <output_file>\n");
+    return EXIT_FAILURE;
+  }
+
+  char * input = argv[1];
+
+  Node * Tree;
+
+  Tree = Load_File(input);
+
+  Tree -> left = NULL;
+
+  return EXIT_SUCCESS;
+}
+
+Node * Load_File(char * input)
+{
+  Stack * s = NULL;
+  Node * n1, * n2, * n3;
+  double width, height;
+  FILE * fptr = fopen(input, "r");
+
+  char a = fgetc(fptr);
+
+  while(a != '\n')
+  {
+    if(a == '(')
+    {
+      fscanf(fptr, "%lf, %lf)", &width, &height);
+      s = push(s, Create_Node(width, height));
+    }
+    else
+    {
+      n1 = pop(s);
+      n2 = pop(s);
+
+      n3 = Create_Node(0, 0);
+      n3 -> left = n2;
+      n3 -> right = n1;
+
+      push(s, n3);
+    }
+
+    a = fgetc(fptr);
+  }
+
+  n3 = pop(s);
+
+  return n3;
+}
+
+int isEmpty(Stack * s)
+{
+  if(s == NULL)
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+Stack * push(Stack * s, Node * n)
+{
+  Stack * s2 = malloc(sizeof(Stack));
+  s2 -> next = s;
+  s2 -> item = n;
+
+  return s2;
+}
+
+Node * Create_Node(double width, double height)
+{
+  Node * n = malloc(sizeof(Node));
+
+  n -> left = NULL;
+  n -> right = NULL;
+  n -> width = width;
+  n -> height = height;
+  n -> x = 0;
+  n -> y = 0;
+
+  return n;
+}
+
+Node * pop(Stack * s)
+{
+  Node * n = s -> item;
+
+  Stack * s2 = s;
+  s = s -> next;
+
+  free(s2);
+
+  return n;
+}
---
+++ PA04/reroot.h
@@ -0,0 +1,25 @@
+// Avoid double inclusion
+#ifndef REROOT_H
+#define REROOT_H 
+
+typedef struct _node {
+  double x, y, width, height;
+  struct _node * left;
+  struct _node * right;
+} Node;
+
+typedef struct _item {
+  struct _item * next;
+  Node * item;
+} Stack;
+
+// Used for file I/O
+Node * Load_File(char *);
+
+// Stack operations
+int isEmpty(Stack *);
+Stack * push(Stack *, Node *);
+Node * pop(Stack *);
+
+Node * Create_Node(double, double);
+#endif /* REROOT_H */
