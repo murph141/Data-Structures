9fc352f4a2762db7975c3df337c669e012b27dc2
Eric Murphy <murph141@purdue.edu>
2014-04-24 18:21:20 +0000

    Cannot create graph

--- PA05/river.c
+++ PA05/river.c
@@ -1,3 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "river.h"
+
+int main(int argc, char * argv[])
+{
+  if(argc != 2)
+  {
+    printf("Usage: proj5 <input_file>\n");
+    return EXIT_FAILURE;
+  }
+
+  char * input = argv[1];
+
+  Node * data = Load_File(input);
+
+  if(data == NULL)
+  {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS; // WE'VE SUCCEEDED!!!!!
+}
+
+
+Node * Load_File(char * input)
+{
+  FILE * fptr = fopen(input, "r");
+
+  if(fptr == NULL)
+  {
+    printf("Error: Memory Allocation Failed!\n");
+    return NULL;
+  }
+
+  int num;
+
+  fscanf(fptr, "%d", &num);
+
+  Node * data;
+
+  data = Create_Graph(num, fptr);
+
+  fclose(fptr);
+
+  return data;
+}
+
+
+Node * Create_Graph(int num, FILE * fptr)
+{
+  return NULL;
+}
--- PA05/river.h
+++ PA05/river.h
@@ -1,4 +1,15 @@
 #ifndef RIVER_H
 #define RIVER_H
 
+typedef struct _node {
+  struct _node * tl;
+  struct _node * tr;
+  struct _node * bl;
+  struct _node * br;
+  int plank;
+} Node;
+
+Node * Load_File(char *);
+Node * Create_Graph(int, FILE *);
+
 #endif // RIVER_H
